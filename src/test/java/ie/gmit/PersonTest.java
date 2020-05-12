df284fd2ee5998cda26542b397af0e2987943e9a
Ringo <pawelwedl25@gmail.com>
2020-05-12 18:51:43 +0100

    Refactor classes Fix tests

--- src/test/java/ie/gmit/PersonTest.java
+++ src/test/java/ie/gmit/PersonTest.java
@@ -5,9 +5,6 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
-
-
 public class PersonTest {
     Person person;
 
@@ -29,55 +26,32 @@ public class PersonTest {
     @DisplayName("Testing constructor initialisation success")
     @Test
     void testingConstructorSuccess() {
-        person = new Person("Mr", "Ruairi Doherty", "NZ23AMZ3", "Contract", "33 Briarhill",123456789,23);
+        person = new Person("Mr", "Ruairi Doherty", "33 Briarhill", 123456789);
         assertEquals("Mr", person.getTitle());
         assertEquals("Ruairi Doherty", person.getName());
         assertEquals(123456789, person.getPhone());
-        assertEquals(23, person.getAge());
-        assertEquals("Contract", person.getType());
         assertEquals("33 Briarhill", person.getAddress());
-        assertEquals("NZ23AMZ3", person.getPps());
     }
 
     @DisplayName("Testing invalid title entry")
     @Test
     void testingInvalidTitle() {
-        Exception e = assertThrows(IllegalArgumentException.class, () -> person = new Person("M", "Random Name", "NGGGGGGG", "Contract", "120 Roscam", 123456789,44));
+        Exception e = assertThrows(IllegalArgumentException.class, () -> person = new Person("M", "Random Name", "NGGGGGGG", 123456789));
         assertEquals("Invalid title provided", e.getMessage());
     }
 
     @DisplayName("Testing invalid name entry")
     @Test
     void testingInvalidName() {
-        Exception e = assertThrows(IllegalArgumentException.class, () -> person = new Person("Mr", "Ru", "NZ23AMZ23", "Contract", "130 Doughiska", 123456789,23));
+        Exception e = assertThrows(IllegalArgumentException.class, () -> person = new Person("Mr", "Ru", "NZ23AMZ23", 123456789));
         assertEquals("Name must contain 3 - 25 characters", e.getMessage());
-    }
-
-    @DisplayName("Testing invalid pps entry")
-    @Test
-    void testingInvalidPPS() {
-        Exception e = assertThrows(IllegalArgumentException.class, () -> person = new Person("Mr", "Ruandria", "NZ", "Contract", "94 Claremorris", 123456789,23));
-        assertEquals("PPS must contain 8 characters", e.getMessage());
-    }
-
-    @DisplayName("Testing invalid type entry")
-    @Test
-    void testingInvalidType() {
-        Exception e = assertThrows(IllegalArgumentException.class, () -> person = new Person("Mr", "Ruairi Doherty", "NZ23AM23", "none", "69 GMIT", 123456789,23));
-        assertEquals("Must contain employment type", e.getMessage());
     }
 
     @DisplayName("Testing invalid phone entry")
     @Test
     void testingInvalidPhone() {
-        Exception e = assertThrows(IllegalArgumentException.class, () -> person = new Person("Mr", "Ruairi Doherty", "NZ23AMZ3", "Part-time", "88 Ros Cah Hill", 1234569,23));
+        Exception e = assertThrows(IllegalArgumentException.class, () -> person = new Person("Mr", "Ruairi Doherty", "NZ23AMZ3", 12345678));
         assertEquals("Phone number must be 9 digits", e.getMessage());
     }
 
-    @DisplayName("Testing invalid age entry")
-    @Test
-    void testingInvalidAge() {
-        Exception e = assertThrows(IllegalArgumentException.class, () -> person = new Person("Mr", "Ruairi Doherty", "NZ23AZ23", "Contract", "11 Salthill", 123456789,13));
-        assertEquals("Employee age must be over 16 to work in the company", e.getMessage());
-    }
 }
