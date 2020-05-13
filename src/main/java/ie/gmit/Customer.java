b62721225693d8821cd70ab43e960f9a1c4913f2
Arnis <arnis.vasilenoks@gmail.com>
2020-05-13 23:12:00 +0100

    add  more functionality

--- src/main/java/ie/gmit/Customer.java
+++ src/main/java/ie/gmit/Customer.java
@@ -4,16 +4,23 @@ import java.math.BigDecimal;
 
 public class Customer extends Person {
     private double balance;
+    private double order;
+    private double credit;
 
 
-    public Customer(String title, String name, String address, String email, long phone, double balance) {
+    public Customer(String title, String name, String address, String email, long phone, double balance, double order, double credit) {
         super(title, name, address, email, phone);
         setBalance(balance);
+        setCredit(credit);
+        setOrder(order);
+        processOrder(credit, balance,order);
 
     }
-    public double getBalance() {
-        return balance;
-    }
+    public double getBalance() { return balance;}
+    public double getOrder() {return order;}
+    public double getCredit() {return credit; }
+
+
 
 
     public void setBalance(double balance) {
@@ -26,4 +33,33 @@ public class Customer extends Person {
                 throw new IllegalArgumentException("Balance must be in decimal(0.00) format");
 
     }
+    public void setOrder(double order) {
+        Double foo = order * 100;
+        foo = foo - foo.intValue();
+        if (foo.compareTo(0D) == 0) {
+            this.order = order;
+        }
+        else
+            throw new IllegalArgumentException("Order value must be in decimal(0.00) format");
+
+    }
+    public void setCredit(double credit) {
+        Double foo = credit * 100;
+        foo = foo - foo.intValue();
+        if (foo.compareTo(0D) == 0) {
+            this.credit = credit;
+        }
+        else
+            throw new IllegalArgumentException("Credit value must be in decimal(0.00) format");
+
+    }
+    public void processOrder(double credit, double balance, double order) {
+        if (credit < order+balance ) {
+            throw new IllegalArgumentException("Insuficient funds");
+        }
+        else
+            this.balance  = balance+order;
+
+    }
+
 }
--- src/test/java/ie/gmit/CustomerTest.java
+++ src/test/java/ie/gmit/CustomerTest.java
@@ -26,22 +26,44 @@ public class CustomerTest {
     @DisplayName("Testing constructor initialisation success")
     @Test
     void testingConstructorSuccess() {
-        Customer = new Customer("Mr", "Ruairi Doherty", "33 Briarhill", "wow@gmail.com", 123456789, 2000);
+        Customer = new Customer("Mr", "Ruairi Doherty", "33 Briarhill", "wow@gmail.com", 123456789, 2000,1000,4000);
         assertEquals("Mr", Customer.getTitle());
         assertEquals("Ruairi Doherty", Customer.getName());
         assertEquals(123456789, Customer.getPhone());
         assertEquals("33 Briarhill", Customer.getAddress());
-        assertEquals(2000, Customer.getBalance());
+        assertEquals(3000, Customer.getBalance());
         assertEquals("wow@gmail.com", Customer.getEmail());
+        assertEquals(1000, Customer.getOrder());
+        assertEquals(4000, Customer.getCredit());
 
     }
 
     @DisplayName("Testing invalid balance")
     @Test
     void testingInvalidBalance() {
-        Exception e = assertThrows(IllegalArgumentException.class, () -> Customer = new Customer("Mr", "Ruairi Doherty", "NZ23AZ23", "wow@gmail.com", 123456789, 2.111));
+        Exception e = assertThrows(IllegalArgumentException.class, () -> Customer = new Customer("Mr", "Ruairi Doherty", "NZ23AZ23", "wow@gmail.com", 123456789, 2.111,2000,4000));
         assertEquals("Balance must be in decimal(0.00) format", e.getMessage());
     }
+
+    @DisplayName("Testing invalid credit")
+    @Test
+    void testingInvalidCredit() {
+        Exception e = assertThrows(IllegalArgumentException.class, () -> Customer = new Customer("Mr", "Ruairi Doherty", "NZ23AZ23", "wow@gmail.com", 123456789, 2.11,2000,4000.111));
+        assertEquals("Credit value must be in decimal(0.00) format", e.getMessage());
+    }
+    @DisplayName("Testing invalid order process")
+    @Test
+    void testingInvalidProcess() {
+        Exception e = assertThrows(IllegalArgumentException.class, () -> Customer = new Customer("Mr", "Ruairi Doherty", "NZ23AZ23", "wow@gmail.com", 123456789, 2.11,2000,1000));
+        assertEquals("Insuficient funds", e.getMessage());
+    }
+    @DisplayName("Testing invalid order ")
+    @Test
+    void testingInvalidOrder() {
+        Exception e = assertThrows(IllegalArgumentException.class, () -> Customer = new Customer("Mr", "Ruairi Doherty", "NZ23AZ23", "wow@gmail.com", 123456789, 2.11,2000.111,1000));
+        assertEquals("Order value must be in decimal(0.00) format", e.getMessage());
+    }
+
 
 
 }
